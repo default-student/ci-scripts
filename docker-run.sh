b50f1ceb5b23225eed3774ad4b6f05af78172c93
LH6626 <l.h.6626@protonmail.com>
2024-08-07 12:51:23 +0200

    run with mapping port and base image name

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Check if the file path argument is provided
 if [ -z "$1" ]; then
   echo "Usage: $0 <path-to-Dockerfile> [env-file1] [env-file2] ..."
@@ -9,7 +10,21 @@ fi
 DOCKERFILE_PATH="$1"
 
 # Extract the image name from the Dockerfile name
-IMAGE_NAME=$(basename "$DOCKERFILE_PATH" | sed 's/^Dockerfile\.//')
+CONTAINER_NAME=$(basename "$DOCKERFILE_PATH" | sed 's/^Dockerfile\.//')
+
+if [ -f docker.env ]; then
+  export $(cat docker.env | grep -v '^#' | xargs)
+  IMAGE_NAME="$BASE_IMAGE_NAME$CONTAINER_NAME:latest"
+  echo "Running Image with Base Image Name $IMAGE_NAME"
+
+  echo "Mapping Port '$PORT' to '$PORT' for the container"
+else
+  echo "Error: No ./docker.env file found"
+  echo "Running Image without Base Image Name $IMAGE_NAME"
+  IMAGE_NAME="$CONTAINER_NAME:latest"
+
+  echo "Not Passing any Port"
+fi
 
 # Check if the image name extraction worked
 if [ -z "$IMAGE_NAME" ]; then
@@ -18,9 +33,10 @@ if [ -z "$IMAGE_NAME" ]; then
 fi
 
 # Stop and remove the running container if it exists
-if docker ps -a --format '{{.Names}}' | grep -Eq "^${IMAGE_NAME}$"; then
-  docker stop "$IMAGE_NAME"
-  docker rm "$IMAGE_NAME"
+if docker ps -a --format '{{.Names}}' | grep -Eq "^${CONTAINER_NAME}$"; then
+  echo "Stopping and removing the existing container '$CONTAINER_NAME'"
+  docker stop "$CONTAINER_NAME"
+  docker rm "$CONTAINER_NAME"
 fi
 
 # Build the Docker image
@@ -32,7 +48,9 @@ if [ "$#" -gt 1 ]; then
   for ENV_FILE in "${@:2}"; do
     ENV_ARGS+=("--env-file" "$ENV_FILE")
   done
+else
+  echo "No environment files provided."
 fi
 
 # Run the Docker container with or without environment files
-docker run --name "$IMAGE_NAME" "${ENV_ARGS[@]}" "$IMAGE_NAME"
+docker run --name "$CONTAINER_NAME" -p "$PORT:$PORT" "${ENV_ARGS[@]}" "$IMAGE_NAME"
